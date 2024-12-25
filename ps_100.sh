0bd4fca2bad258ac8dd7253504c57947af63d679
mcampita <mcampita@student.42.fr>
2024-12-25 17:19:02 +0200

    edited show arg when over limit

--- ps_100.sh
+++ ps_100.sh
@@ -6,10 +6,13 @@ total=100
 
 for i in $(seq 1 $total)
 do
-  ARG=$(seq 1 500 | shuf | tr '\n' ' ')
+  ARG=$(seq 1 100 | shuf | tr '\n' ' ')
   result=$(./push_swap $ARG | wc -l)
   if (( result > max )); then
     max=$result
+	if (( max > 700 )); then
+      echo "ARG when max > 700: $ARG"
+    fi
   fi
   if (( result < min )); then
     min=$result
@@ -18,9 +21,10 @@ do
   echo -ne "Progress: $progress% \r"
 done
 
-echo -e "\nLowest value: $min"
-if (( max < 5500 )); then
+#echo -e "\nLowest value: $min"
+if (( max < 700 )); then
   echo -e "\e[32mHighest value: $max\e[0m"
 else
   echo -e "\e[31mHighest value: $max\e[0m"
 fi
+
